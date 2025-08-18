bae79c04fef8ad60cd1aa392b050407eeb8b5591
Moustapha FEKOTAN <moustphafek@gmail.com>
2025-08-18 14:53:38 +0100

    Test réussi

--- database/factories/LivresFactory.php
+++ database/factories/LivresFactory.php
@@ -5,6 +5,7 @@ namespace Database\Factories;
 use App\Models\Categories;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Str;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Livres>
@@ -25,6 +26,12 @@ class LivresFactory extends Factory
               'slug'  => (string) Str::uuid(),
                'categorie_id' =>  Categories::factory(),
                 'user_id' => User::factory(),
+                 // ✅ cover optionnelle pour tests
+    'cover' => function() {
+        // Génère un faux fichier image pour la factory
+        $fakeImage = UploadedFile::fake()->image('cover.jpg');
+        return $fakeImage->store('covers', 'public'); // chemin relatif
+    },
         ];
     }
 }
