5e77168b53136ca2b2c92adf07f0ec05eb1162b7
Moustapha FEKOTAN <moustphafek@gmail.com>
2025-08-20 23:58:55 +0100

    Nouvelle configuration pour le deploiement de l'api sur render

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -7,13 +7,8 @@ php artisan scribe:generate
 echo "=== Cache des configurations ==="
 php artisan config:cache
 
-echo "=== Suppression du cache des routes Scribe pour éviter 404 sur /docs ==="
-php artisan route:clear
-
-echo "=== Mise en cache des routes normales ==="
-# Ici on met en cache toutes les routes sauf celles qui posent problème
-# En général, Laravel ne permet pas d'exclure des routes, donc on mettra le cache après avoir généré la doc
-php artisan route:cache || echo "Certaines routes (ex: docs) ne peuvent pas être mises en cache. Ignoré."
+# echo "=== Cache des routes ==="
+# php artisan route:cache
 
 echo "=== Exécution des migrations ==="
 php artisan migrate --force
@@ -21,4 +16,3 @@ php artisan migrate --force
 echo "=== Remplissage de la base de données ==="
 php artisan db:seed
 
-echo "=== Déploiement terminé ==="
