d2a3d1407aad81074bc0f02d94d61c37283a7d41
Moustapha FEKOTAN <moustphafek@gmail.com>
2025-08-21 19:29:50 +0100

    Nouvelle configuration pour le deploiement de l'api sur render

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Middleware\HandleCors;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        api: __DIR__.'/../routes/api.php',
+        web: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
@@ -27,3 +27,4 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->create();
+ 
